d168efc6337241144f2ada97fdfbb7d86db52bfe
Lushi Li <lushili06@gmail.com>
2013-05-16 14:39:17 -0400

    Created Models

--- yakster_app/models.py
+++ yakster_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
+class Yakster(models.Model):
+    title = models.CharField(max_length=60)
+    body = models.TextField()
+    user = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def gravatar_url(self):
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
